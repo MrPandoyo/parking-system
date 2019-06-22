f5b2221b820146e077cc9f460b35eb0a520b7ef9
Iqbal Pandoyo <iqbalpandoyo@gmail.com>
2019-06-22 17:04:54 +0700

    apus librariry yg gk dipke

--- src/main/resources/static/custom/number-validator.js
+++
@@ -1,9 +0,0 @@
-function isNumberKey(evt) {
-    var e = evt || window.event;
-    var charCode = e.which || e.keyCode;
-    if (charCode > 31 && (charCode < 47 || charCode > 57))
-        return false;
-    if (e.shiftKey)
-        return false;
-    return true;
-}
